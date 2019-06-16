258b682e485a6d94d9a0a82c680002506464de6a
Rodrigo <rodrigoclimao@gmail.com>
2019-06-16 15:39:56 -0400

    adding information and a different nav bar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Rodrigos Portfolio' });
 });
 
 router.get('/home', (req, res, next) => {
