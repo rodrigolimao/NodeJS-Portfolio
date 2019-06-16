299c247b3d228649fdb148106d9d100405381c4f
Rodrigo <rodrigoclimao@gmail.com>
2019-06-16 14:47:58 -0400

    created a simple nav bar with links

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/home', (req, res, next) => {
+  res.render('home', {});
+});
+
+router.get('/aboutme', (req, res, next) => {
+  res.render('aboutme', {});
+});
+
+router.get('/projects', (req, res, next) => {
+  res.render('projects', {});
+});
+
+router.get('/services', (req, res, next) => {
+  res.render('services', {});
+});
+
+router.get('/contactme', (req, res, next) => {
+  res.render('contactme', {});
+});
+
 module.exports = router;
