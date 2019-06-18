9acff30757e49fca5c5bcf892d7c8bda6e99128f
Rodrigo <rodrigoclimao@gmail.com>
2019-06-17 22:28:14 -0400

    adding some css and relevant info to the website

--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Rodrigos Portfolio' });
 });
 
+/* GET home page. */
 router.get('/home', (req, res, next) => {
   res.render('home', {});
 });
 
+/* GET About me page. */
 router.get('/aboutme', (req, res, next) => {
   res.render('aboutme', {});
 });
 
+/* GET Projects page. */
 router.get('/projects', (req, res, next) => {
   res.render('projects', {});
 });
 
+/* GET Services page. */
 router.get('/services', (req, res, next) => {
   res.render('services', {});
 });
 
+/* GET Contact me page. */
 router.get('/contactme', (req, res, next) => {
   res.render('contactme', {});
 });
