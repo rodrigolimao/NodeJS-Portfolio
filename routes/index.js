d171a9dda0a7509b516f158c49b45670e7e721bf
Rodrigo <rodrigoclimao@gmail.com>
2019-06-24 15:01:16 -0400

    installing contact form packages and commenting

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+//Nodemailer variables
+const nodemailer = require('nodemailer');
+const app = express();
+
+//Npm package body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+// POST route from contact form
+app.post('/contactme', function (req, res) {
+  let mailOpts, smtpTrans;
+  smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: GMAIL_USER,
+      pass: GMAIL_PASS
+    }
+  });
+
+  mailOpts = {
+    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+    to: GMAIL_USER,
+    subject: 'New message from contact form at rodrigolima.ca',
+    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+  };
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+    if (error) {
+      res.render('contact-failure');
+    }
+    else {
+      res.render('contact-success');
+    }
+  });
+});
+
+/* GET Index page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Rodrigos Portfolio' });
 });
@@ -32,3 +69,6 @@ router.get('/contactme', (req, res, next) => {
 });
 
 module.exports = router;
+
+/*Contact form using NodeMailer package and body parser package retrieved from the website:
+https://tylerkrys.ca/blog/adding-nodemailer-email-contact-form-node-express-app from Tyler Krys */
